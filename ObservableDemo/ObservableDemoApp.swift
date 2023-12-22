3d4d43b3f2b35d74912a72fe59e041d17358bf83
Jaeseong Jeong <js104427@gmail.com>
2023-12-22 18:02:17 +0800

    Initial Commit

---
+++ ObservableDemo/ObservableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableDemoApp.swift
+//  ObservableDemo
+//
+//  Created by Jaeseong Jeong on 22/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ObservableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
